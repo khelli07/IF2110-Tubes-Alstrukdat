75ce507f5bd4e942b0693118a2e183cba46e1089
dxt99 <ericlouis19x@gmail.com>
2021-10-26 22:16:44 +0800

    temporary mobita definitions

---
+++ mobita/mobita.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include "mobita.h"
+#include "../boolean.h"
+#include "../location/location.h"
+//#include "../ability/ability.h"
+//#include "../linkedlist/linkedlist.h"
+//#include "../staticlist/staticlist.h"
+//#include "../dynamiclist/dynamiclist.h"
+//#include "../adjmat/adjmat.h"
+//#include "../map/map.h"
+
+/* KONSTRUKTOR */
+void CreateMobita(Mobita* m){
+	//Creates base mobita
+	BALANCE(*m)=0;
+	LOCATION(*m)={1,1}; //will be switched to hq coords
+	ABILITY(*m)=0; //will be switched to default ability
+	TODO(*m)=NULL; //empty linked list
+	INPROGRESS(*m)=NULL; //empty linked list
+	StaticList s; CreateStaticList(&s);
+	INVENTORY(*m)=s; //empty static list
+	DynamicList d; CreateDynamicList(&d);
+	BUILDINGLIST(*m)=d;
+	Adjmat adj; CreateAdjMat(&adj);
+	ADJMAT(*m)=adj;
+	Map map; CreateMap(&map)
+	MAP(*m)=map;
+}
---
+++ mobita/mobita.h
@@ -0,0 +1,68 @@
+// TEMPORARY SUMMARY OF MOBITA DS; SUBJECT TO CHANGe
+
+#ifndef MOBITA_H
+#define MOBITA_H
+
+#include "../location/location.h"
+//#include "../ability/ability.h"
+//#include "../linkedlist/linkedlist.h"
+//#include "../staticlist/staticlist.h"
+//#include "../dynamiclist/dynamiclist.h"
+//#include "../adjmat/adjmat.h"
+//#include "../map/map.h"
+
+//extras
+#include "../pesanan/pesanan.h"
+
+typedef struct{
+	int balance;
+    Location curLoc;
+    Ability curAbility;
+    LinkedList toDoList;
+    LinkedList inProgressList;
+    StaticList inventory;
+    DynamicList buildingList;
+    Adjmat adjMatrix;
+    Map map;
+} Mobita;
+
+/* SELEKTOR */
+#define BALANCE(m) (m).balance
+#define LOCATION(m) (m).curLoc
+#define ABILITY(m) (m).curAbility
+#define TODO(m) (m).toDoList
+#define INPROGRESS(m) (m).inProgressList
+#define INVENTORY(m) (m).inventory
+#define BUILDINGLIST(m) (m).buildingList
+#define ADJMAT(m) (m).adjMatrix
+#define MAP(m) (m).map
+
+/* KONSTRUKTOR */
+void CreateMobita(Mobita* m);
+
+/* COMMANDS (SPEC SUBJECT TO CHANGE) */ 
+void CommandMove(Mobita *m, Location loc);
+// moves mobita to loc (updates LOCATION(m))
+
+void CommandPickUp(Mobita *m, Pesanan P);
+// moves P from toDoList to inProgressList
+
+void CommandDropOff(Mobita *m, Pesanan P);
+// deletes P from inProgressList, adds balance
+
+void CommandMap(Mobita m);
+// displays map
+
+void CommandToDo(Mobita m);
+// displays to do list
+
+void CommandInProgress(Mobita m);
+// displays in progress list (picked up items)
+
+void CommandBuy(Mobita* m);
+// buys gadget, parameters still unclear
+
+void CommandInventory(Mobita m);
+// displays inventory
+
+#endif
